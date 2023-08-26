b7a39c04603199174c1316dc7abd1743cc7b29e4
Chan <33cks1423@naver.com>
2023-08-26 18:00:44 +0900

    [#2] 리팩토링

--- src/main/java/org/leetcode/validpalindrome/Solution.java
+++ src/main/java/org/leetcode/validpalindrome/Solution.java
@@ -2,28 +2,48 @@ package org.leetcode.validpalindrome;
 
 import java.util.StringJoiner;
 
-public class Solution {
-    public boolean isPalindrome(String s) {
+public final class Solution {
+
+    /**
+     * 이 클래스는 인스턴스를 만들 수 없습니다.
+     */
+    private Solution() {
+        throw new AssertionError();
+    }
+
+    public static boolean isPalindrome(String s) {
+        checkNull(s);
+
         if (s.isEmpty()) {
             return true;
         }
         int start = 0;
         int last = s.length() - 1;
+
         while(start <= last) {
             char currFirst = s.charAt(start);
             char currLast = s.charAt(last);
-            if (!Character.isLetterOrDigit(currFirst )) {
+            if (!Character.isLetterOrDigit(currFirst)) {
                 start++;
-            } else if(!Character.isLetterOrDigit(currLast)) {
-                last--;
-            } else {
-                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-                    return false;
-                }
-                start++;
-                last--;
+                continue;
             }
+            if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+                continue;
+            }
+
+            if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                return false;
+            }
+            start++;
+            last--;
         }
         return true;
+    }
+
+    private static void checkNull(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("s cannot be null");
+        }
     }
 }
