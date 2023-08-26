9110497ae41bc65df91312b85bac19e202ab1f07
Chan <33cks1423@naver.com>
2023-08-26 20:40:10 +0900

    [#3] 리팩토링

--- src/main/java/org/leetcode/longest_substring_without_reapeating_characters/Solution.java
+++ src/main/java/org/leetcode/longest_substring_without_reapeating_characters/Solution.java
@@ -6,23 +6,38 @@ import java.util.Map;
 
 public class Solution {
 
-    private final Map<Character, Integer> storage = new LinkedHashMap<>();
-    public int lengthOfLongestSubstring(String s) {
-        if (s == null || s.isEmpty()) {
+    /**
+     * 이 클래스는 인스턴스를 만들 수 없습니다.
+     */
+    private Solution() {
+        throw new AssertionError();
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        checkNull(s);
+         Map<Character, Integer> storage = new HashMap<>();
+        if (s.isEmpty()) {
             return 0;
         }
-        int max = 0;
-        storage.clear();
 
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
+        int max = 0;
+        int left = 0;
+        int right = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
             if (storage.containsKey(c)) {
-                int index = storage.get(c);
-                storage.entrySet().removeIf(entry -> entry.getValue() <= index);
+                left = Math.max(left, storage.get(c) + 1);
             }
-            storage.put(c, i);
-            max = Math.max(max, storage.size());
+            storage.put(c, right);
+            max = Math.max(max, right - left + 1);
+            right++;
         }
         return max;
+    }
+    private static void checkNull(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("s must not be null");
+        }
     }
 }
--- src/test/java/org/leetcode/longest_substring_without_reapeating_characters/SolutionTest.java
+++ src/test/java/org/leetcode/longest_substring_without_reapeating_characters/SolutionTest.java
@@ -1,22 +1,33 @@
 package org.leetcode.longest_substring_without_reapeating_characters;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SolutionTest {
 
-    private final Solution solution = new Solution();
 
     @ParameterizedTest
-    @CsvSource({"abcabcbb, 3", "bbbbb, 1", "pwwkew, 3", " , 0", "dvdf, 3", "abba, 2"})
+    @CsvSource({"abcabcbb, 3", "bbbbb, 1", "pwwkew, 3", "dvdf, 3", "abba, 2"})
     @DisplayName("lengthOfLongestSubstring 메서드는 문자열 s 를 받아 최대 부분 문자열의 길이를 반환한다.")
     void test(String s, int expected) {
 
-        int actual = solution.lengthOfLongestSubstring(s);
+        int actual = Solution.lengthOfLongestSubstring(s);
 
         assertThat(expected).isEqualTo(actual);
+    }
+
+    @Test
+    @DisplayName("lengthOfLongestSubstring 메서드는 문자열 s 가 null 일 때 IllegalArgumentException 을 던진다.")
+    void test2() {
+        String s = null;
+
+        assertThatThrownBy(() -> Solution.lengthOfLongestSubstring(s))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("s must not be null");
     }
 }
