d13395b5a2d727dc2890faa34ddea50c3257351a
Chan <33cks1423@naver.com>
2023-08-27 20:30:38 +0900

    [#5] 프로덕션 코드 작성

--- src/main/java/org/leetcode/search_a_2d_matrix/Solution.java
+++ src/main/java/org/leetcode/search_a_2d_matrix/Solution.java
@@ -3,7 +3,29 @@ package org.leetcode.search_a_2d_matrix;
 public class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         checkNull(matrix);
+        if (matrix.length == 0) {
+            return false;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
 
+        int left = 0;
+        int right = row * col - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int midValue = matrix[mid / col][mid % col];
+
+            if (midValue < target) {
+                left = mid + 1;
+                continue;
+            }
+            if (midValue > target){
+                right = mid - 1;
+                continue;
+            }
+            return true;
+        }
         return false;
     }
 
