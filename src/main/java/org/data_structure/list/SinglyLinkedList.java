0da1bb390cdde37bf5b45377329d008a03a4133f
Chan <33cks1423@naver.com>
2023-09-10 23:56:35 +0900

    [#12] 링크드 리스트

---
+++ src/main/java/org/data_structure/list/SinglyLinkedList.java
@@ -0,0 +1,85 @@
+package org.data_structure.list;
+
+public class SinglyLinkedList<E> {
+    private Node<E> head;
+    private int size;
+
+    public SinglyLinkedList() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public void add(int index, E data) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            addFirst(data);
+        } else {
+            Node<E> prev = getNode(index - 1);
+            addAfter(prev, data);
+        }
+    }
+
+    public void addFirst(E data) {
+        head = new Node<>(data, head);
+        size++;
+    }
+
+    public void addAfter(Node<E> prev, E data) {
+        prev.next = new Node<>(data, prev.next);
+        size++;
+    }
+
+    public Node<E> getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+
+        return node;
+    }
+
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            return removeFirst();
+        }
+
+        Node<E> prev = getNode(index - 1);
+        return removeAfter(prev);
+    }
+
+    public E removeFirst() {
+        if (head == null) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E data = head.data;
+        head = head.next;
+        size--;
+
+        return data;
+    }
+
+    public E removeAfter(Node<E> prev) {
+        if (prev.next == null) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        E data = prev.next.data;
+        prev.next = prev.next.next;
+        size--;
+
+        return data;
+    }
+}
